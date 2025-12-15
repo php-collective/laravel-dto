84b5aee3afb402df658bc63ae3f81178f52803ca
mscherer <dereuromark@web.de>
2025-12-15 14:36:44 +0100

    Fix IoInterface implementation

--- src/LaravelConsoleIo.php
+++ src/LaravelConsoleIo.php
@@ -14,23 +14,77 @@ class LaravelConsoleIo implements IoInterface
     ) {
     }
 
-    public function out(string $message): void
+    /**
+     * @inheritDoc
+     */
+    public function verbose(array|string $message, int $newlines = 1): ?int
     {
+        if (is_array($message)) {
+            $message = implode(PHP_EOL, $message);
+        }
+        $this->command->line($message, null, 'v');
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function quiet(array|string $message, int $newlines = 1): ?int
+    {
+        if (is_array($message)) {
+            $message = implode(PHP_EOL, $message);
+        }
+        $this->command->line($message, null, 'quiet');
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function out(?string $message = null, int $newlines = 1, int $level = self::NORMAL): ?int
+    {
+        if ($message === null) {
+            return null;
+        }
         $this->command->line($message);
+
+        return null;
     }
 
-    public function success(string $message): void
+    /**
+     * @inheritDoc
+     */
+    public function error(?string $message = null, int $newlines = 1): ?int
     {
+        if ($message === null) {
+            return null;
+        }
+        $this->command->error($message);
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function success(?string $message = null, int $newlines = 1, int $level = self::NORMAL): ?int
+    {
+        if ($message === null) {
+            return null;
+        }
         $this->command->info($message);
+
+        return null;
     }
 
-    public function warning(string $message): void
-    {
-        $this->command->warn($message);
-    }
-
-    public function error(string $message): void
+    /**
+     * @inheritDoc
+     */
+    public function abort(string $message, int $exitCode = 1): void
     {
         $this->command->error($message);
+        exit($exitCode);
     }
 }
